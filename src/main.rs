8201da2c322c922e724089dc13c8431c2ba5cf07
SkyfallWasTaken <mahadkalam1@proton.me>
2024-05-27 11:44:28 +0100

    DATA and RSET commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use color_eyre::Result;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -14,7 +16,8 @@ struct ConnState {
 
     mail_from: Option<String>,
     rcpt_to: Vec<String>,
-    data: Option<[u8; consts::MAX_SIZE]>,
+    waiting_for_data: bool,
+    data: Option<Vec<u8>>, // We can't use a &[u8], as that could cause a stack overflow
 }
 
 async fn process(mut socket: TcpStream) {
@@ -26,6 +29,7 @@ async fn process(mut socket: TcpStream) {
 
         mail_from: None,
         rcpt_to: Vec::new(),
+        waiting_for_data: false,
         data: None,
     };
 
@@ -42,67 +46,107 @@ async fn process(mut socket: TcpStream) {
         };
 
         let msg = String::from_utf8_lossy(&buf[0..n]);
-        let command = Command::from_smtp_message(&msg).unwrap();
-        match command {
-            Command::Helo { fqdn } => {
-                state.greeting_done = true;
-                state.esmtp = false;
-                socket.write_all(messages::HELO_RESPONSE).await.unwrap();
-            }
-            Command::Ehlo { fqdn } => {
-                state.greeting_done = true;
-                state.esmtp = true;
-                socket.write_all(messages::HELO_RESPONSE).await.unwrap();
 
-                for ext in esmtp::SUPPORTED_EXTENSIONS {
-                    socket
-                        .write_all(format!("250-{}\n", ext).as_bytes())
-                        .await
-                        .unwrap();
-                }
+        if state.waiting_for_data {
+            // TODO: Implement dot stuffing
+            if msg.ends_with("\r\n.\r\n") {
+                state.waiting_for_data = false;
+                state
+                    .data
+                    .get_or_insert_with(Vec::new)
+                    .extend_from_slice(&buf[0..n - 5]); // Don't include the trailing \r\n.\r\n
+                socket.write_all(messages::OK).await.unwrap();
+            } else {
+                state
+                    .data
+                    .get_or_insert_with(Vec::new)
+                    .extend_from_slice(&buf[0..n]);
             }
-            Command::MailFrom { email } => {
-                if !state.greeting_done {
-                    socket
-                        .write_all(messages::BAD_COMMAND_SEQUENCE)
-                        .await
-                        .unwrap();
-                    continue;
+        } else {
+            let Some(command) = Command::from_smtp_message(&msg) else {
+                socket
+                    .write_all(messages::UNRECOGNIZED_COMMAND)
+                    .await
+                    .unwrap();
+                continue;
+            };
+            match command {
+                Command::Helo { fqdn } => {
+                    state.greeting_done = true;
+                    state.esmtp = false;
+                    socket.write_all(messages::HELO_RESPONSE).await.unwrap();
+                }
+                Command::Ehlo { fqdn } => {
+                    state.greeting_done = true;
+                    state.esmtp = true;
+                    socket.write_all(messages::HELO_RESPONSE).await.unwrap();
+
+                    for ext in esmtp::SUPPORTED_EXTENSIONS {
+                        socket
+                            .write_all(format!("250-{}\n", ext).as_bytes())
+                            .await
+                            .unwrap();
+                    }
                 }
 
-                state.mail_from = Some(email.to_string());
-                socket.write_all(messages::OK).await.unwrap();
-            }
-            Command::RcptTo { email } => {
-                if !state.greeting_done || state.mail_from.is_none() {
-                    socket
-                        .write_all(messages::BAD_COMMAND_SEQUENCE)
-                        .await
-                        .unwrap();
-                    continue;
+                Command::MailFrom { email } => {
+                    if !state.greeting_done {
+                        socket
+                            .write_all(messages::BAD_COMMAND_SEQUENCE)
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+
+                    state.mail_from = Some(email.to_string());
+                    socket.write_all(messages::OK).await.unwrap();
+                }
+                Command::RcptTo { email } => {
+                    if !state.greeting_done || state.mail_from.is_none() {
+                        socket
+                            .write_all(messages::BAD_COMMAND_SEQUENCE)
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+
+                    state.rcpt_to.push(email.to_string());
+                    socket.write_all(messages::OK).await.unwrap();
+                }
+                Command::Data => {
+                    if !state.greeting_done || state.mail_from.is_none() || state.rcpt_to.is_empty()
+                    {
+                        socket
+                            .write_all(messages::BAD_COMMAND_SEQUENCE)
+                            .await
+                            .unwrap();
+                        continue;
+                    }
+
+                    state.waiting_for_data = true;
+                    socket.write_all(messages::DATA_RESPONSE).await.unwrap();
                 }
 
-                state.rcpt_to.push(email.to_string());
-                socket.write_all(messages::OK).await.unwrap();
-            }
-            Command::Help => {
-                socket.write_all(messages::HELP_RESPONSE).await.unwrap();
-            }
-            Command::NoOp => {
-                socket.write_all(messages::OK).await.unwrap();
-            }
-            Command::Rset => {
-                state.mail_from = None;
-                state.rcpt_to.clear();
-                state.data = None;
-                socket.write_all(messages::OK).await.unwrap();
+                Command::Help => {
+                    socket.write_all(messages::HELP_RESPONSE).await.unwrap();
+                }
+                Command::NoOp => {
+                    socket.write_all(messages::OK).await.unwrap();
+                }
+                Command::Rset => {
+                    state.mail_from = None;
+                    state.rcpt_to.clear();
+                    state.data = None;
+                    socket.write_all(messages::OK).await.unwrap();
+                }
             }
         }
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    color_eyre::install()?;
     tracing_subscriber::fmt::init();
     let listener = TcpListener::bind(ADDR).await.unwrap();
 
@@ -112,6 +156,7 @@ async fn main() {
         let (socket, _) = listener.accept().await.unwrap();
         // A new task is spawned for each inbound socket. The socket is
         // moved to the new task and processed there.
+        socket.set_nodelay(true).unwrap();
         tokio::spawn(async move {
             process(socket).await;
         });
--- src/messages.rs
+++ src/messages.rs
@@ -8,15 +8,20 @@ pub const GREETING: &[u8] = formatcp!(
 pub const HELO_RESPONSE: &[u8] = b"250-smtp2.example.org ready when you are, [$hostname]\n";
 pub const BAD_COMMAND_SEQUENCE: &[u8] = b"503 Bad sequence of commands\n";
 pub const OK: &[u8] = b"250 OK\n";
+pub const DATA_RESPONSE: &[u8] = b"354 End data with <CR><LF>.<CR><LF>\n";
 pub const HELP_RESPONSE: &[u8] =
     b"214-go check out https://datatracker.ietf.org/doc/html/rfc5321\n";
+pub const UNRECOGNIZED_COMMAND: &[u8] = b"500 Unrecognized command\n";
 
 #[derive(Debug)]
 pub enum Command<'a> {
     Helo { fqdn: &'a str },
     Ehlo { fqdn: &'a str },
-    MailFrom { email: &'a str },
-    RcptTo { email: &'a str },
+
+    MailFrom { email: String },
+    RcptTo { email: String },
+    Data,
+
     Help,
     NoOp,
     Rset,
@@ -24,7 +29,7 @@ pub enum Command<'a> {
 
 impl<'a> Command<'a> {
     pub fn from_smtp_message(msg: &'a str) -> Option<Command<'a>> {
-        let msg = msg.split_whitespace().collect::<Vec<&str>>();
+        let msg: Vec<&str> = msg.trim().split_whitespace().collect();
         let cmd = msg.first()?.to_uppercase();
         let cmd = cmd.as_str();
 
@@ -33,19 +38,36 @@ impl<'a> Command<'a> {
             "EHLO" => Some(Self::Ehlo { fqdn: msg.get(1)? }),
             "MAIL" => {
                 let arg = msg.get(1)?.to_uppercase();
+
                 if arg.starts_with("FROM:") {
-                    let email = arg[6..].trim_start_matches('<').trim_end_matches('>');
+                    let email = arg
+                        .trim_start_matches('<')
+                        .trim_end_matches('>')
+                        .to_string();
 
                     Some(Self::MailFrom { email })
                 } else {
                     None
                 }
             }
-            "RCPT" if msg.get(1)?.to_uppercase() == "TO" => {
-                Some(Self::RcptTo { email: msg.get(2)? })
+            "RCPT" => {
+                let arg = msg.get(1)?.to_uppercase();
+
+                if arg.starts_with("TO:") {
+                    let email = arg
+                        .trim_start_matches('<')
+                        .trim_end_matches('>')
+                        .to_string();
+
+                    Some(Self::RcptTo { email })
+                } else {
+                    None
+                }
             }
+            "DATA" => Some(Self::Data),
             "HELP" => Some(Self::Help),
             "NOOP" => Some(Self::NoOp),
+            "RSET" => Some(Self::Rset),
             _ => None,
         }
     }
