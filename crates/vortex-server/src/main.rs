63395f96757583ac4d1f9e5663da9383c275aa53
SkyfallWasTaken <mahadkalam1@proton.me>
2024-06-18 22:26:29 +0100

    change smtp port to 2525

--- crates/vortex-server/src/main.rs
+++ crates/vortex-server/src/main.rs
@@ -8,7 +8,7 @@ use tokio::net::TcpListener;
 use vortex_smtp::{event::Event, Email};
 
 const HTTP_ADDR: &str = "0.0.0.0:3000";
-const SMTP_ADDR: &str = "0.0.0.0:25";
+const SMTP_ADDR: &str = "0.0.0.0:2525";
 
 type EmailsMap = DashMap<String, Vec<Email>>;
 
