7ea0866cadd71161515b675e085e122307a3d335
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-26 22:09:19 +0100

    Fix compile error

--- crates/vortex-smtp/src/esmtp.rs
+++ crates/vortex-smtp/src/esmtp.rs
@@ -1,6 +1,6 @@
 use const_format::concatcp;
 
-pub const SUPPORTED_EXTENSIONS: &[&str; 2] = &[
+pub const SUPPORTED_EXTENSIONS: &[&str; 1] = &[
     // "HELP",
     concatcp!("SIZE ", crate::consts::MAX_SIZE),
 ];
