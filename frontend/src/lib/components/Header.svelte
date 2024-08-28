54becf3cfffb0290284604ce250be1c98ba405af
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-28 01:31:50 +0100

    Fix broken link

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -5,7 +5,7 @@
 <div
 	class="border-b-2 border-zinc-300 px-3 py-2 font-semibold dark:border-slate-800 dark:text-white"
 >
-	Made with ❤️ by <a href="https://skyfall.dino.icu" target="_blank" class="underline">Skyfall</a>
+	Made with ❤️ by <a href="https://skyfall.dev" target="_blank" class="underline">Skyfall</a>
 	<div class="float-right">
 		<LightSwitch />
 	</div>
