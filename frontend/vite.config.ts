14a128b1c834ddf2a04489b95f803fb1fe393367
SkyfallWasTaken <mahadkalam1@proton.me>
2024-09-03 03:58:20 +0100

    Formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "zephyr-labs",
-            project: "vortex"
-        }
-    }), sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'zephyr-labs',
+				project: 'vortex'
+			}
+		}),
+		sveltekit()
+	],
 	envDir: '../',
 	build: {
 		rollupOptions: {
