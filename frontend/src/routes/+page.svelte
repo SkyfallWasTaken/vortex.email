a862207a0dd918ae61b62788fc889248ee4c7d82
SkyfallWasTaken <mahadkalam1@proton.me>
2024-09-03 01:45:04 +0100

    Formatting

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -76,13 +76,13 @@
 		<div>
 			{#if $query.isLoading}
 				<div
-					class="light-bg dark:bg-surface-500 flex items-center justify-center rounded-md p-6 shadow-sm"
+					class="light-bg flex items-center justify-center rounded-md p-6 shadow-sm dark:bg-surface-500"
 				>
 					<p class="text-lg font-semibold">One sec...</p>
 				</div>
 			{:else if $query.isError}
 				<div
-					class="light-bg dark:bg-surface-500 flex flex-col items-center justify-center rounded-md p-6 shadow-sm"
+					class="light-bg flex flex-col items-center justify-center rounded-md p-6 shadow-sm dark:bg-surface-500"
 				>
 					<h2 class="text-lg font-semibold">Uh oh, something went wrong</h2>
 					<p>Sorry about that! Please refresh the page and try again.</p>
