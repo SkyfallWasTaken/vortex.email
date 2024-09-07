bd3f0761c875e2a4342a3f92122896e8778faa07
SkyfallWasTaken <mahadkalam1@proton.me>
2024-09-07 13:54:02 +0100

    Add credits page

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -3,10 +3,18 @@
 </script>
 
 <div
-	class="border-b-2 border-zinc-300 px-3 py-2 font-semibold dark:border-slate-800 dark:text-white"
+	class="flex flex-row border-b-2 border-zinc-300 px-3 py-2 font-semibold dark:border-slate-800 dark:text-white"
 >
-	Made with ❤️ by <a href="https://skyfall.dev" target="_blank" class="underline">Skyfall</a>
-	<div class="float-right">
+	<div class="flex flex-row gap-2">
+		<div>
+			Made with ❤️ by <a href="https://skyfall.dev" target="_blank" class="underline">Skyfall</a>
+		</div>
+		<div>•</div>
+		<div>
+			<a href="/credits" class="underline">Credits</a>
+		</div>
+	</div>
+	<div class="ml-auto">
 		<LightSwitch />
 	</div>
 </div>
---
+++ frontend/src/routes/credits/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import ArrowRightIcon from 'lucide-svelte/icons/arrow-right';
+</script>
+
+<svelte:head>
+	<title>Credits - Vortex</title>
+</svelte:head>
+
+<div class="container mx-auto flex w-4/5 flex-col justify-center gap-4 p-4 lg:w-1/2">
+	<div>
+		<h1 class="mb-2 text-3xl font-bold">Credits</h1>
+		<p>Thank you to the following people for their contributions to Vortex:</p>
+	</div>
+	<ul class="list-disc">
+		<li>
+			<a href="https://pixelverse.tech/" target="_blank" class="underline">Hayden Kong/Pixelverse</a
+			>
+			for the
+			<b>shadow.pixelverse.tech, assembly.pixelverse.tech, and corporate.pixelverse.tech</b> subdomains
+		</li>
+		<li>
+			<a href="https://hackclub.com/" target="_blank" class="underline">Hack Club</a> for the
+			<b>shadow.dino.icu, erdtree.dino.icu, and vortex.dino.icu</b> subdomains
+		</li>
+		<li>
+			<a href="https://mattsoh.dev/" target="_blank" class="underline">Matthew Soh</a> for the
+			<b>vortex.mattsoh.dev</b> subdomain
+		</li>
+	</ul>
+	<hr class="my-4" />
+	<a href="/" class="btn border-gray-300 p-1">
+		<ArrowRightIcon size="1.2rem" class="mr-2" />
+		<p>Go back home...</p>
+	</a>
+</div>
