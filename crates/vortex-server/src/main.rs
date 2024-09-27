e75932e2eddc072c3a82b114c55b0878b9e92a9b
SkyfallWasTaken <mahadkalam1@proton.me>
2024-09-27 22:50:02 +0100

    Fix file appender

--- crates/vortex-server/src/main.rs
+++ crates/vortex-server/src/main.rs
@@ -123,20 +123,17 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     let log_dir = env::var("LOG_DIR").wrap_err("failed to read env var LOG_DIR")?;
+    let appender = tracing_appender::rolling::daily(log_dir, "vortex-server.log");
+    let (non_blocking_appender, _log_guard) = tracing_appender::non_blocking(appender);
     tracing_subscriber::Registry::default()
         .with(sentry::integrations::tracing::layer())
-        .with(
-            tracing_subscriber::fmt::layer().with_writer(tracing_appender::rolling::daily(
-                log_dir,
-                "vortex-server.log",
-            )),
-        )
+        .with(tracing_subscriber::fmt::layer().with_writer(non_blocking_appender))
         .with(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
     let sentry_dsn =
         env::var("VITE_SENTRY_DSN").wrap_err("failed to read env var VITE_SENTRY_DSN")?;
-    let _guard = sentry::init((
+    let _sentry_guard = sentry::init((
         sentry_dsn,
         sentry::ClientOptions {
             release: sentry::release_name!(),
