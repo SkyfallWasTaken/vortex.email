de6099fdfc04dc187910f3de0513ce9a67d1ca98
Mahad Kalam <mahadkalam1@proton.me>
2025-01-11 19:41:43 +0000

    Implement copy and generate buttons

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -3,10 +3,12 @@ import type { Email } from "../email";
 
 import { Letter } from 'react-letter';
 import { extract } from 'letterparser';
+import { Copy, CopyCheck, RefreshCcw } from 'lucide-react';
 
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { useDebounce } from "@uidotdev/usehooks";
+import { useDebounce, useCopyToClipboard } from "@uidotdev/usehooks";
+import { fakerEN as faker } from '@faker-js/faker';
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -25,12 +27,20 @@ export function Email({ email }: { email: Email }) {
   )
 }
 
+function getRandomEmailDomain(emailDomains: string[]) {
+  return emailDomains[Math.floor(Math.random() * emailDomains.length)];
+}
+
 export default function Home() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState(faker.internet.username());
   const debouncedUsername = useDebounce(username, 400);
 
   const emailDomains: string[] = import.meta.env.VITE_EMAIL_DOMAINS.split(',');
-  const [emailDomain, setEmailDomain] = useState(emailDomains[0]);
+  const randomDomain = getRandomEmailDomain(emailDomains);
+  const [emailDomain, setEmailDomain] = useState(randomDomain);
+
+  const [copied, setCopied] = useState(false);
+  const [copiedContents, setCopiedContents] = useCopyToClipboard();
 
   const { isPending, error, data } = useQuery<Email[]>({
     queryKey: ['emails', debouncedUsername, emailDomain],
@@ -52,13 +62,38 @@ export default function Home() {
           <h1 className="text-6xl font-bold">Free, disposable email addresses</h1>
           <p className="text-xl">For annoying newsletters, websites, and everything in between! Protect your privacy and avoid spam with temporary email addresses.</p>
         </div>
-        <div className="flex justify-center items-center w-1/3 mx-auto">
-          <input type="text" className="h-12 mx-auto mt-6 p-4 focus:border-none focus:outline-none focus:ring-[1px] focus:ring-mauve rounded" placeholder="Enter your email address" onChange={(e) => setUsername(e.target.value)} />
-          <select name="email-domain" className="h-12 mx-auto mt-6 p-4 focus:border-none focus:outline-none focus:ring-[1px] focus:ring-mauve rounded" value={emailDomain} onChange={(e) => setEmailDomain(e.target.value)}>
-            {emailDomains.map((domain) => (
-              <option key={domain} value={domain}>@{domain}</option>
-            ))}
-          </select>
+        <div>
+          <div className="flex justify-center items-center w-1/3 mx-auto mb-2.5">
+            <input type="text" className="h-12 mx-auto mt-6 p-4 focus:border-none focus:outline-none focus:ring-[1px] focus:ring-mauve rounded" placeholder="Enter your email address" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <select name="email-domain" className="h-12 mx-auto mt-6 p-4 focus:border-none focus:outline-none focus:ring-[1px] focus:ring-mauve rounded" value={emailDomain} onChange={(e) => setEmailDomain(e.target.value)}>
+              {emailDomains.map((domain) => (
+                <option key={domain} value={domain}>@{domain}</option>
+              ))}
+            </select>
+          </div>
+          <div className="flex gap-4 text-overlay1 justify-center items-center w-1/3 mx-auto">
+            <button className="flex items-center space-x-2" onClick={() => {
+              setCopiedContents(debouncedUsername + "@" + emailDomain);
+              setCopied(true);
+              setTimeout(() => {
+                setCopied(false);
+              }, 1000);
+            }}>
+              {copied ? (
+                <CopyCheck size={16} />
+              ) : (
+                <Copy size={16} />
+              )}
+              <span>{copied ? 'Copied!' : 'Copy email'}</span>
+            </button>
+            <button className="flex items-center space-x-2" onClick={() => {
+              setUsername(faker.internet.username());
+              setEmailDomain(getRandomEmailDomain(emailDomains));
+            }}>
+              <RefreshCcw size={16} />
+              <span>Generate new email</span>
+            </button>
+          </div>
         </div>
       </div>
       <div>
@@ -68,7 +103,7 @@ export default function Home() {
           data.map((email) => (
             <Email key={email.id} email={email} />
           ))
-        ) : <p className="text-center mt-8">No emails found.</p>}
+        ) : <div className="text-center py-5 bg-surface0">No emails found for {debouncedUsername}@{emailDomain}</div>}
       </div>
     </>
   );
