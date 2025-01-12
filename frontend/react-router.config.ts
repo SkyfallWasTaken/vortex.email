292e19608210cd29f7cc941ddafdd4ed9c6ae628
Mahad Kalam <mahadkalam1@proton.me>
2025-01-12 13:53:46 +0000

    Formatting

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Vercel doesn't support SSR :skull:
-  ssr: false,
+	// Vercel doesn't support SSR :skull:
+	ssr: false,
 } satisfies Config;
